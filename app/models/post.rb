e3dbb43150ff3435b35072b9210995601f27d805
Tanattha <tanattha.thuenim@gmail.com>
2020-06-14 22:52:33 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ActiveRecord::Base
-    validates :title, presence: true
+    include ActiveModel::Validations
+    validates :title, presence: true, :exclusion => { :in => %w(Won't Believe Secret Top [0-9]* Guess) }
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-    validates :title, length: { minimum: 15 }
+    validates_with ClickbaitValidator
+
+
+    
 end
