fdc97e475ea165cd6d285e26f9f585e633c29003
Tanattha Thuenim <60161395+Tanattha@users.noreply.github.com>
2020-06-15 10:21:46 -0400

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
     include ActiveModel::Validations
-    validates :title, presence: true, :exclusion => { :in => %w(Won't Believe Secret Top [0-9]* Guess) }
+    validates :title, presence: true
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
