fb826b54313b036b24adf7fdfeb6b77827ac7e67
github username <github email address>
2020-06-17 16:29:19 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid category" }
+    validate :clickbait
+
+    def clickbait
+        clickbait = Regexp.new("(Won't Believe)|(Top) \d*|(Secret)|(Guess)")
+        if title && !title.match?(clickbait)
+          errors.add(:title, "That's not clickbait!")
+        end
+    end
+
 end
