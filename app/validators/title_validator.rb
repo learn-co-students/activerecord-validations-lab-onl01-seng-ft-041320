d104430dfdb62d220ad58dcb7ae1e4a17757ec26
Franklyn Rodriguez <franklynr92@gmail.com>
2020-06-23 00:23:32 -0400

    Done.

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -2,7 +2,7 @@ class TitleValidator < ActiveModel::Validator
 
     #[Won't Believe|Secret|Top*[0-9]|Guess]
 def validate(record)
-    unless record.match?([ /Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i ])
+    unless record.title.match?([ /Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i ])
         record.errors[:title] << "must be click bait"
     end
 end
