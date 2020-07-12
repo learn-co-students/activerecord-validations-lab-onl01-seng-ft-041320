268d74b50e9660486201a2f360362f8803f7401e
jaesonwatts <illender.the.great@gmail.com>
2020-07-12 12:38:05 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum:250}
+  validates :summary, length: {maximum:250}
+  validates :category, inclusion: { in: %w(Non-Fiction Fiction),
+    message: "%{value} is not a valid category" }
+
+  validate :ensures_title_is_clickbait?
+
+
+  CLICKBAIT_TITLES = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+    
+  def ensures_title_is_clickbait?
+    if CLICKBAIT_TITLES.none? { |t| t.match title }
+      errors.add(:title, "needs more cowbell")
+    end
+  end
+
 end
+
+
